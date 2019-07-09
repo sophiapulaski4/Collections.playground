b1d02fc16c213e6a0731d51bfc062001ce97fb88
caroline dugan <caroline.dugan@gmail.com>
2019-07-09 13:51:44 -0700

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//var arrayOfStrings = [String]()
+//
+//var arrayOfIntegers = [int]()
+
+// Michelle Obama has an index of zero
+//Jimmy fallon has an index of 3
+//There are four names, or four elements but we count starting from zero for collections
+var friendsOfKarlie = ["Michelle Obama", "Serena Williams", "Taylor Swift", "Jimmy Fallon"]
+
+print(friendsOfKarlie[1])
+
+// Cannot have different data types in arrays
+//var mixedDataTypes = [1, "Michelle Obama", 10]
